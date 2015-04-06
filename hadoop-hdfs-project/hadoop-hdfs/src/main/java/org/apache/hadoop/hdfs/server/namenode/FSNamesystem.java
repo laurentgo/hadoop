9e0f3de90a46bff1728a532ac9e866349678243a
Zhe Zhang <zhz@apache.org>
2015-04-06 10:37:23 -0700

    HDFS-7936. Erasure coding: resolving conflicts in the branch when merging trunk changes (this commit is for HDFS-8035). Contributed by Zhe Zhang

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -3136,7 +3136,7 @@ public class FSNamesystem implements Namesystem, FSNamesystemMBean,
    */
   LocatedBlock storeAllocatedBlock(String src, long fileId, String clientName,
       ExtendedBlock previous, DatanodeStorageInfo[] targets) throws IOException {
-    BlockInfo newBlockInfo = null;
+    Block newBlock = null;
     long offset;
     checkOperation(OperationCategory.WRITE);
     waitForLoadingFSImage();
@@ -3169,8 +3169,8 @@ public class FSNamesystem implements Namesystem, FSNamesystemMBean,
                                 ExtendedBlock.getLocalBlock(previous));
 
       // allocate new block, record block locations in INode.
-      Block newBlock = createNewBlock(isStriped);
-      newBlockInfo = saveAllocatedBlock(src, fileState.iip, newBlock, targets,
+      newBlock = createNewBlock(isStriped);
+      saveAllocatedBlock(src, fileState.iip, newBlock, targets,
           isStriped);
 
       persistNewBlock(src, pendingFile);
@@ -3181,7 +3181,7 @@ public class FSNamesystem implements Namesystem, FSNamesystemMBean,
     getEditLog().logSync();
 
     // Return located block
-    return makeLocatedBlock(newBlockInfo, targets, offset);
+    return makeLocatedBlock(getStoredBlock(newBlock), targets, offset);
   }
 
   /*
