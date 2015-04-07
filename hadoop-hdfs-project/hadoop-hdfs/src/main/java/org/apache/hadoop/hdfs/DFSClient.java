06987aaf522ffad65295daaf8f004a4f3f347dfb
Zhe Zhang <zhz@apache.org>
2015-04-07 11:20:13 -0700

    HDFS-7782. Erasure coding: pread from files in striped layout. Contributed by Zhe Zhang and Jing Zhao

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSClient.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSClient.java
@@ -3156,7 +3156,7 @@ public class DFSClient implements java.io.Closeable, RemotePeerFactory,
       LOG.debug("Using hedged reads; pool threads=" + num);
     }
   }
-  
+
   /**
    * Create thread pool for parallel reading in striped layout,
    * STRIPED_READ_THREAD_POOL, if it does not already exist.
