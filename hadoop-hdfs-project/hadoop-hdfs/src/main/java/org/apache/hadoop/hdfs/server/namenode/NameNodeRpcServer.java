bf2f940da788c6d81ae3abf788aca9eb027493ba
Vinayakumar B <vinayakumarb@apache.org>
2015-04-20 15:04:49 +0530

    HDFS-8181. createErasureCodingZone sets retryCache state as false always (Contributed by Uma Maheswara Rao G)

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNodeRpcServer.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/NameNodeRpcServer.java
@@ -1854,6 +1854,7 @@ class NameNodeRpcServer implements NamenodeProtocols {
     boolean success = false;
     try {
       namesystem.createErasureCodingZone(src, schema, cacheEntry != null);
+      success = true;
     } finally {
       RetryCache.setState(cacheEntry, success);
     }
